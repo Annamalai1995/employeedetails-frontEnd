21c2f52caf4fa2f24a5064a537f95cd8ce044628
Annamalai1995 <s.annamalai1995@gmail.com>
2022-11-17 10:29:04 +0530

    Create and list is completed

--- src/Home.js
+++ src/Home.js
@@ -39,11 +39,43 @@ export const Homepage=()=>
                     setCreateView(true)
                 }
             }>
-                Create a neww User
+                Create a new User
             </button>
-            <></>
+            <div className="row justify-content-center">
+                        <div className="table-responsive-md">
+                            <table className="col-lg-8 col-md-10 col-sm-12 table table-striped  p-4 shadow rounded">
+                                <thead>
+                                    <tr>
+                                        <th>Employee Id</th>
+                                        <th>Employee Name</th>
+                                        <th>Employee Username</th>
+                                        <th>Employee Password</th>
+                                        <th>Employee Designation</th>
+                                        <th>Employee Experience</th>
+                                        <th>Employee Salary</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        tmparray.map((ele,ind)=>
+                                        (
+                                            <tr>
+                                            <td>{ele.empId}</td>
+                                            <td>{ele.empName}</td>
+                                            <td>{ele.empUsername}</td>
+                                            <td>{ele.empPassword}</td>
+                                            <td>{ele.empDesignation}</td>
+                                            <td>{ele.empExp}</td>
+                                            <td>{ele.empSalary}</td>     
+                                            </tr>
 
-            
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+         
             </>
             }
             
